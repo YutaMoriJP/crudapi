d13be20ca7fde24c838d92b8dc6f4352ca9ae60f
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-08-05 18:17:25 -0700

    more style updates

--- src/styled/Select.js
+++ src/styled/Select.js
@@ -2,14 +2,10 @@ import styled from "styled-components";
 import ReactSelect from "react-select";
 
 const Select = styled(ReactSelect)`
-  width: 90%;
-  max-width: 400px;
-  min-width: 150px;
-  /*
-  width: 250px;
-  @media screen and (max-width: 250px) {
+  width: 280px;
+  @media screen and (max-width: 500px) {
     width: 180px;
-  } */
+  }
 `;
 
 export default Select;
